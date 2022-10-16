e00f78a28e23e816a7ccf4e713a8efa0b5863e33
Jiin <j00j12i11@duksung.ac.kr>
2022-10-16 18:57:52 +0900

    add explanation in DH_int__.cpp

--- Diffie-Hellman/DH_int__.cpp
+++ Diffie-Hellman/DH_int__.cpp
@@ -49,12 +49,28 @@ int main(){
 }
 
 void to_bits(vector<int>& bits, int x){
+    /* 
+        x를 받아 bits에 2진수로 저장한다.
+        x를 2로 나누어 나오는 나머지를 차례대로 저장하고, x의 몫을 x에 저장해 다음 반복으로 넘긴다.
+        제일 처음 나머지가 가장 오른쪽에 위치해야 하므로, 항상 벡터의 맨 앞에 새로운 값을 넣어준다.
+        즉 11의 경우, bits에 [1, 0, 1, 1]이 저장되는 것과 같다.
+    */
     while (x > 0){
         bits.insert(bits.begin(), x % 2);
         x /= 2;
     }
 }
 int powm(int base, int exp, int mod){
+    /* 
+        base^exp % mod 연산을 진행하는 함수
+        exp가 클 경우에 직접 지수승 한 뒤에 연산을 하는것은 매우 오래걸리며 비효율적이다.
+        따라서, 지수를 2진수로 표현하여 mod연산을 중간중간 하며 효율적으로 연산할 수 있다.
+        2진수로 표현했을 때, 1일경우에는 base를 곱하고 모드연산을하고 자리수를 하나 올릴때에는 제곱을 한뒤 모드연산을 한다.
+        0일 경우에는 자리수를 올리는 것만 하고 모드연산을 한다.
+
+        만약 11 = 0x1011의 경우, (((1*base % mod)^2 * base % mod)^2 % mod)^2 * base % mod)
+        -> 곱 모드 제곱, 곱 모드 제곱, 모드 제곱, 곱 모드 제곱
+    */
     vector<int> bits; 
     to_bits(bits, exp);
 
@@ -68,6 +84,13 @@ int powm(int base, int exp, int mod){
     return result;
 }
 bool is_primitive(int base, int mod){
+    /*
+        base % mod == base^n % mod 가 될 때까지 반복한다.
+        모듈러 연산을 1승부터 +1씩 하면, mod-1개의 값들이 반복적으로 나타나며 한주기를 돌때까지 같은 값은 나오지 않는다.
+        즉 base % mod == base^n % mod일 때,  n == mod라면 1부터 mod-1의 수가 모두 있다는 뜻이 된다.
+        (모드상에서 나올 수 있는 수는 1~mod-1이고, base^1 % mod와 base^mod % mod가 같으므로 주기가 mod-1이다.)
+        (원시근은 모드 p상에서 지수승 연산의 결과가 1~p-1의 값들이 주기적으로 나타나는 것이므로,,, 원시근 확인 가능)
+    */
     int tmp = base; // 'base' is same as 'base % mod'
     int i = 1;
     do{
@@ -78,6 +101,10 @@ bool is_primitive(int base, int mod){
     return i == mod;
 }
 void primitive_root(vector<int>& p_roots, int mod){
+    /*
+        2부터 mod까지 모든 수들에 대해 is_primitive함수로 원시근인지 판별함.
+        원시근은 p_roots 벡터에 저장
+    */
     for (int i = 2; i < mod; i++){
         if(is_primitive(i, mod))
             p_roots.push_back(i);
